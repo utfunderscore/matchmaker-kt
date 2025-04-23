6c3750a790358f033fc16125d4cc03c8abfe518e
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-04-23 14:39:58 +0100

    feat: Add joinedAt timestamp and mark socketId as JsonIgnore in QueueTeam data class

--- Server/src/main/kotlin/org/readutf/matchmaker/queue/QueueTeam.kt
+++ Server/src/main/kotlin/org/readutf/matchmaker/queue/QueueTeam.kt
@@ -1,11 +1,13 @@
 package org.readutf.matchmaker.queue
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.JsonNode
 import java.util.UUID
 
 data class QueueTeam(
     val teamId: UUID,
-    var socketId: String,
+    @JsonIgnore val joinedAt: Long = System.nanoTime(),
+    @JsonIgnore val socketId: String,
     val players: List<UUID>,
     val attributes: JsonNode,
 )
